5f8f8d9d4ef8872c8cdb21a6e16c36c1ae31fe66
TauVip <Abylai1393@gmail.com>
2020-12-21 18:57:10 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import { Component } from 'react'
+import { connect } from 'react-redux'
+import { setBooks } from './actions/books'
+import books from './books.json'
+
+class App extends Component {
+  render() {
+    // const { books } = this.props
+    return (
+      <ul>
+        {books.map(book => (
+          <li>
+            <b>{book.title}</b> - {book.author}
+          </li>
+        ))}
+      </ul>
+    )
+  }
+}
+
+const mapStateToProps = ({ books }) => ({
+  books: books.items
+})
+
+const mapDispatchToProps = dispatch => ({
+  setBooks: books => dispatch(setBooks(books))
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(App)
+
+// Пишем интернет-магазин на ReactJS + Redux #1 | 1:44:37
